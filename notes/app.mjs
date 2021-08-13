ef5202c13b708a3f797ddb48772c36cb453f1cd1
knubbe <knubbe1982@gmail.com>
2021-08-13 07:50:49 +0200

    implement bootstrap

--- notes/app.mjs
+++ notes/app.mjs
@@ -27,6 +27,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(dirname, 'public')));
+app.use('/assets/vendor/bootstrap', express.static(path.join(dirname, 'node_modules', 'bootstrap', 'dist')));
+app.use('/assets/vendor/jquery', express.static(path.join(dirname, 'node_modules', 'jquery', 'dist')));
+app.use('/assets/vendor/popper.js', express.static(path.join(dirname, 'node_modules', 'popper.js', 'dist', 'umd')));
 
 app.use('/', indexRouter);
 app.use('/notes', notesRouter);
