4ede2e9cba6e07daf61a049b217e7aeed861b7f1
knubbe <knubbe1982@gmail.com>
2021-08-28 17:40:23 +0200

    finalize debug

--- notes/appsuport.mjs
+++ notes/appsuport.mjs
@@ -1,5 +1,9 @@
 import { port } from "./app.mjs";
 import { server } from "./app.mjs";
+import { default as DBG } from 'debug';
+
+const debug = DBG('notes:debug');
+const dbgerror = DBG('notes:error');
 
 /**
  * Normalize a port into a number, string, or false.
@@ -7,17 +11,17 @@ import { server } from "./app.mjs";
 
  export function normalizePort(val) {
     const port = parseInt(val, 10);
-  
+
     if (isNaN(port)) {
       // named pipe
       return val;
     }
-  
+
     if (port >= 0) {
       // port number
       return port;
     }
-  
+
     return false;
   }
 
@@ -26,14 +30,15 @@ import { server } from "./app.mjs";
  */
 
 export function onError(error) {
+    dbgerror(error);
     if (error.syscall !== 'listen') {
       throw error;
     }
-  
+
     const bind = typeof port === 'string'
       ? 'Pipe ' + port
       : 'Port ' + port;
-  
+
     // handle specific listen errors with friendly messages
     switch (error.code) {
       case 'EACCES':
@@ -48,15 +53,15 @@ export function onError(error) {
         throw error;
     }
   }
-  
+
   /**
    * Event listener for HTTP server "listening" event.
    */
-  
+
   export function onListening() {
     const addr = server.address();
     const bind = typeof addr === 'string'
       ? 'pipe ' + addr
       : 'port ' + addr.port;
-    console.log('Listening on ' + bind);
+    debug(`Listening on ${bind}`);
   }
